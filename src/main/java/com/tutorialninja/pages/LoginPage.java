2747c60df96e6a582164319b0fde2ca6b7fcbcfe
rimadas <rimadas@localhost>
2025-03-01 17:07:43 -0800

    Adding tests to checkforgetpassword, back actions, verify placeholders

--- src/main/java/com/tutorialninja/pages/LoginPage.java
+++ src/main/java/com/tutorialninja/pages/LoginPage.java
@@ -1,41 +1,55 @@
 package com.tutorialninja.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import com.tutorialninja.basepage.BasePage;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
-		
+
 	}
-	
-	@FindBy(linkText="My Account")
-	private WebElement myaccount;
-	
-	@FindBy(linkText="Login")
-	private WebElement loginBtn;
-	
-	@FindBy(name="email")
+
+//	@FindBy(linkText = "My Account")
+//	private WebElement myaccount;
+//
+//	@FindBy(linkText = "Login")
+//	private WebElement loginBtn;
+
+	@FindBy(name = "email")
 	private WebElement emailAddress;
-	
-	@FindBy(name="password")
+
+	@FindBy(name = "password")
 	private WebElement enterPassword;
-	
-	@FindBy(xpath="//input[@type=\"submit\"]")
+
+	@FindBy(xpath = "//input[@type=\"submit\"]")
 	private WebElement loginToAcct;
 
 	public void login(String emailaddress, String password) {
-		
-		myaccount.click();
-		loginBtn.click();
+
+
 		emailAddress.sendKeys(emailaddress);
 		enterPassword.sendKeys(password);
 		loginToAcct.click();
-		
+
 	}
+	
+	@FindBy(xpath="//*[@id=\"content\"]/div/div[2]/div/form/div[2]/a")
+	private WebElement forgetPassword;
+	
+	@FindBy(xpath="//*[@id=\"content\"]/h1")
+	private WebElement title;
+
+	public void forgetPass() {
+		
+		forgetPassword.click();
+		String pageTitle = title.getText();
+	}
+	
+	
 
 }
