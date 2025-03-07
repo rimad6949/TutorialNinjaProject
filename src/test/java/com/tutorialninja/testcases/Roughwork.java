d5b88efd650dcf0d16eb5c149f608eb2e7197025
rimadas <rimadas@localhost>
2025-03-06 16:32:57 -0800

    Adding testcases for Registration Page

--- src/test/java/com/tutorialninja/testcases/Roughwork.java
+++ src/test/java/com/tutorialninja/testcases/Roughwork.java
@@ -1,9 +1,16 @@
 package com.tutorialninja.testcases;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
 import com.tutorialninja.base.BaseTest;
+import com.tutorialninja.pages.RegistrationPage;
 
 public class Roughwork extends BaseTest{
 
-
-
+	
 }
